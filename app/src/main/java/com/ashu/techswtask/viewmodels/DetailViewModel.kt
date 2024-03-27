3f283e418e383a6029f1679a4b9e149c841b4dab
aashuprasad <aashuprasad603@gmail.com>
2024-03-27 15:45:10 +0530

    updated to latest AGP

--- app/src/main/java/com/ashu/techswtask/viewmodels/DetailViewModel.kt
+++ app/src/main/java/com/ashu/techswtask/viewmodels/DetailViewModel.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.ashu.techswtask.db.Food
 
 class DetailViewModel(foodDetail: Food, app: Application) : AndroidViewModel(app) {
@@ -14,5 +16,17 @@ class DetailViewModel(foodDetail: Food, app: Application) : AndroidViewModel(app
 
     init {
         _selectedFood.value = foodDetail
+    }
+}
+
+class DetailViewModelFactory(
+    private val food: Food,
+    private val application: Application) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+            return DetailViewModel(food, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
